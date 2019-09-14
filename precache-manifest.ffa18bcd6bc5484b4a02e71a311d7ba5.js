813464a4552b398c687b7b01740168fbf920cc36
Astromium <interstellarspace2000@gmail.com>
2019-09-14 17:56:00 +0100

    Updates

---
+++ precache-manifest.ffa18bcd6bc5484b4a02e71a311d7ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69d65619f0142ad2f94dd5c5f294804",
+    "url": "/prayertimes/index.html"
+  },
+  {
+    "revision": "52f6d6d8dffbd5273124",
+    "url": "/prayertimes/static/css/main.e939c9cc.chunk.css"
+  },
+  {
+    "revision": "ca279b168c1d88f3fafa",
+    "url": "/prayertimes/static/js/2.197dee30.chunk.js"
+  },
+  {
+    "revision": "52f6d6d8dffbd5273124",
+    "url": "/prayertimes/static/js/main.09410ebe.chunk.js"
+  },
+  {
+    "revision": "c13d26666ede5dd39082",
+    "url": "/prayertimes/static/js/runtime~main.39707e78.js"
+  }
+]);
